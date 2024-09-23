3d7feeb3bd2f0cee129cca05e9d2515a8897fe3e
YAKgamer <kahwajiamine10@gmail.com>
2024-09-23 12:10:09 +0300

    empty

---
+++ src/main/java/net/yakgamer/stonetrapdoors/StoneTrapdoors.java
@@ -0,0 +1,16 @@
+package net.yakgamer.stonetrapdoors;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StoneTrapdoors implements ModInitializer {
+	public static final String MOD_ID = "stonetrapdoors";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/yakgamer/stonetrapdoors/StoneTrapdoorsClient.java
@@ -0,0 +1,10 @@
+package net.yakgamer.stonetrapdoors;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StoneTrapdoorsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
